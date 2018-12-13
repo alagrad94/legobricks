10ff40e7d91aed2da6dd82ae433a1dd6be1fd6ab
Russell Reiter <russell.reiter@gmail.com>
2018-12-13 11:49:17 -0600

    Third commit

---
+++ src/scripts/data.js
@@ -0,0 +1,12 @@
+
+const data = {
+    postLego (legoToSave) {
+        fetch("http://localhost:8088/legos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(legoToSave)
+        })
+    }
+}
--- src/scripts/eventListeners.js
+++ src/scripts/eventListeners.js
@@ -1,5 +1,17 @@
 const eventListeners = {
     handleFormSubmission () {
-        console.log("button clicked");
+        const creator = document.getElementById("lego__creator").value;
+        const name = document.getElementById("lego__name").value;
+        const shape = document.getElementById("lego__shape").value;
+        const color = document.getElementById("lego__color").value;
+
+        const legoObject = {
+            Creator: creator,
+            Nme: name,
+            Shape: shape,
+            Color: color
+        }
+
+        data.postLego(legoObject);
     }
 }
